ae031e3fa5a1282b4bdd50eb9b29e8ea1d5da576
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-01-31 01:25:51 +0100

    Dailly challenge

---
+++ src/CoinFlip.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+class CoinFlip {
+
+    public static void main(String[] args) {
+    // create a new random number generator object
+    Random rand = new Random();
+
+    // pick a random number between 0 and 1:
+    int n = rand.nextInt(2);
+    System.out.println("Picked random value " + n);
+
+  }
+}
